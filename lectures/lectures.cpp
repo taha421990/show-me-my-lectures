8c7f636301b53186008b87ecf1f68ac2b876d502
thirstyferret <thirstyferret@2bbd25d0-92eb-11dd-8be0-476c5aa1ec3f>
2008-10-05 16:05:24 +0000

    Added Curl

--- lectures/lectures.cpp
+++ lectures/lectures.cpp
@@ -1,18 +1,88 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+#include "curl/curl.h"
+#include "curl/easy.h"
+  
+using namespace std;  
+  
+// Write any errors in here  
+static char errorBuffer[CURL_ERROR_SIZE];  
+  
+// Write all expected data in here  
+static string buffer;  
+  
+// This is the writer call back function used by curl  
+static int writer(char *data, size_t size, size_t nmemb,  
+                  std::string *buffer)  
+{  
+  // What we will return  
+  int result = 0;  
+  
+  // Is there anything in the buffer?  
+  if (buffer != NULL)  
+  {  
+    // Append the data to the buffer  
+    buffer->append(data, size * nmemb);  
+  
+    // How much did we write?  
+    result = size * nmemb;  
+  }  
+  
+  return result;  
+}  
+  
+// You know what this does..  
+void usage()  
+{  
+  cout << "curltest: \n" << endl;  
+  cout << "  Usage:  curltest url\n" << endl;  
+}   
+  
+/* 
+ * The old favorite 
+ */  
+int main(int argc, char* argv[])  
+{  
+	string url = "http://beta.chris-alexander.co.uk";  
 
-int main() {
+	cout << "Retrieving " << url << endl;  
 
-	cout << "Hello World!";
+	// Our curl objects  
+	CURL *curl;  
+	CURLcode result;  
 
-	string variable;
+	// Create our curl handle  
+	curl = curl_easy_init();  
 
-	do {
+	if (curl)  
+	{  
+	  // Now set up all of the curl options  
+	  curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer);  
+	  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);  
+	  curl_easy_setopt(curl, CURLOPT_HEADER, 0);  
+	  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);  
+	  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);  
+	  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);  
 
-		cin >> variable;		
+	  // Attempt to retrieve the remote page  
+	  result = curl_easy_perform(curl);  
 
-	} while (variable != "exit");
+	  // Always cleanup  
+	  curl_easy_cleanup(curl);  
 
-}
+	  // Did we succeed?  
+	  if (result == CURLE_OK)  
+	  {  
+		cout << buffer << "\n";  
+		exit(0);  
+	  }  
+	  else  
+	  {  
+		cout << "Error: [" << result << "] - " << errorBuffer;  
+		exit(-1);  
+	  }
+
+	}  
+
+}  
