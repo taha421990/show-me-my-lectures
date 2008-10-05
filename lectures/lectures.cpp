3ecc59163d8eda376ae34225d6f98c417b049e80
thirstyferret <thirstyferret@2bbd25d0-92eb-11dd-8be0-476c5aa1ec3f>
2008-10-05 15:01:27 +0000

    Added the Visual Studio project

---
+++ lectures/lectures.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!";
+
+	string variable;
+
+	do {
+
+		cin >> variable;		
+
+	} while (variable != "exit");
+
+}
